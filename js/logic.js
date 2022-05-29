001f3e7f52d416260755d6df28b6f1a0bdb58145
s82927 <s82927@informatik.htw-dresden.de>
2022-05-29 23:07:40 +0200

    js: Uhrzeitanzeige

--- js/logic.js
+++ js/logic.js
@@ -1,1 +1,10 @@
+function zeigeUhrzeit()
+{
+    var str = "";
+    var now = new Date();
 
+    str = now.toLocaleString();
+
+    document.getElementById("displayTime").innerHTML = str;
+}
+setInterval(zeigeUhrzeit, 1000);
