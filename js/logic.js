c9b49ef6c7eea93d4b51a104a37738122c1fbd71
s82927 <s82927@informatik.htw-dresden.de>
2022-05-29 23:10:47 +0200

    css und js: elemente untereinn. + nur noch Uhrzeit

--- js/logic.js
+++ js/logic.js
@@ -3,7 +3,7 @@ function zeigeUhrzeit()
     var str = "";
     var now = new Date();
 
-    str = now.toLocaleString();
+    str = now.toLocaleTimeString();
 
     document.getElementById("displayTime").innerHTML = str;
 }
