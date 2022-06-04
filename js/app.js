411bd716559db0ae1c0a3e7c9c5e8f77f788ab8d
s82927 <s82927@informatik.htw-dresden.de>
2022-06-04 20:02:22 +0200

    einrichten der Installation des sw

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('/sw.js')
+    //gibt nach erfolgreichem Abschluss promise, danach:
+    .then((reg) => console.log('Service worker erfolgreich registriert', reg))
+    //sonst Fehler loggen:
+    .catch((err) => console.log('Service worker-Registrierung fehlgeschlagen', err));
 }
