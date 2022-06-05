37b3d9c70e4a18b5031dd64d3db3f3d125eb47e0
s82927 <s82927@informatik.htw-dresden.de>
2022-06-05 20:27:37 +0200

    Caching vorbereitet, Cache-Liste übergeben

--- sw.js
+++ sw.js
@@ -1,5 +1,32 @@
+//Name für Cache-Ressource:
+const staticCacheName = "site-static";
+//Liste zu cachender Inhalte:
+const assets = [
+    //Array der möglichen Requests, die gecacht werden sollen
+    "/",     //unspez. index-Page
+    "/index.html",  //index-Page
+    "/js/app.js",
+    "js/logic.js",
+    "css/style.css",
+    "/img/wide.png",
+    "https://fonts.googleapis.com", //Online-Fonts
+    "https://fonts.gstatic.com",
+];
+
 //Vorbereitung + Konsolen-Output
 self.addEventListener("install", event => {
+    
+    //da SW-Install vielleicht früher beendet als Caching:
+    event.waitUntil(
+
+        //Ressourcen-Caching (asynchron)
+        caches.open(staticCacheName).then(cache => {
+            
+            console.log("Elemente gecacht.");
+            //Array von Ressourcen vom Server abrufen und in Cache ablegen:
+            cache.addAll(assets);
+        })
+    );
     console.log("Service Worker wurde installiert");
 });
 
