452309b4f0ac9ae0c4fc659c537cf558a9c38f63
gelatinfossils <d-bchr@protonmail.com>
2024-07-26 00:52:03 +0200

    refactor: Update WebGL shader program to include texture mapping and mouse interaction

--- js/src/testShader/testFragSh.glsl
+++ js/src/testShader/testFragSh.glsl
@@ -11,23 +11,30 @@ uniform sampler2D uSampler;
 out vec4 fragColor;
 
 void main() {
+    float t = uTime;
     
-    uTime;
     vec2 uv = uResolution;
     // normalize moues position
     vec2 mouse = uMouse.xy;
     float mouseClick = uMouse.z;
 
-    vec3 color = vec3(0.0, 0.0, 0.0);
+    vec3 color = vec3(0.0);
     // show the mouse position
     if (distance(gl_FragCoord.xy, mouse * uv) < 5.0) {
         if (mouseClick == 1.0) {
-            color = vec3(0.0, 0.0, 1.0);
+            color = vec3(1.0, 1.0, 1.0);
         } else {
-            color = vec3(0.0, 1.0, 0.0);
+            color = vec3(0.0118, 0.2314, 0.5059);
         }
     }
-    vec3 texColor = texture(uSampler, vTexCoord).rgb;
-    fragColor = vec4(texColor + color, 1.0);
+    // normalize the texture size with the resolution
+    float scaleFactor = 50.0 / 100000.0;
+    float texWidth = vTexCoord.x * uv.x * scaleFactor;
+    float texHeight = vTexCoord.y * uv.y * scaleFactor;
+    vec2 texCoord = vec2(texWidth, texHeight);
+
+    vec3 texColor = texture(uSampler, texCoord).rgb;
+
+    fragColor = vec4(texColor + color, t);
 }
 
